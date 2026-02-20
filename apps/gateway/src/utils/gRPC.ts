bfd064a1f7dc57e1b1b61e4433c9d0562b5ae4af
Sagar-1103 <sagarshirgaonkar59511@gmail.com>
2026-02-21 01:02:34 +0530

    gRPC imports fixed

--- apps/gateway/src/utils/gRPC.ts
+++ apps/gateway/src/utils/gRPC.ts
@@ -1,21 +1,21 @@
-import grpc from "@grpc/grpc-js";
+import * as grpc from "@grpc/grpc-js";
 
-const GrpcError = async(err:any)=>{
+const GrpcError = async (err: any) => {
     let httpStatus;
-switch (err.code) {
-    case grpc.status.INVALID_ARGUMENT:
-    httpStatus = 400;
-    break;
-    case grpc.status.ALREADY_EXISTS:
-    httpStatus = 409;
-    break;
-    case grpc.status.INTERNAL:
-    default:
-    httpStatus = 500;
-    break;
-}
-const response = {statusCode:httpStatus,message:err.message.split(": ")[1]};
-return response;
+    switch (err.code) {
+        case grpc.status.INVALID_ARGUMENT:
+            httpStatus = 400;
+            break;
+        case grpc.status.ALREADY_EXISTS:
+            httpStatus = 409;
+            break;
+        case grpc.status.INTERNAL:
+        default:
+            httpStatus = 500;
+            break;
+    }
+    const response = { statusCode: httpStatus, message: err.message.split(": ")[1] };
+    return response;
 }
 
-export {GrpcError};
+export { GrpcError };
